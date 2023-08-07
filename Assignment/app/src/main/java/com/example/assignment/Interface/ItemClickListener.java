2ed05bdda1cb1e3ce2d23b123c58a4342c50c4cd
Aojiro <93260485+aojiro@users.noreply.github.com>
2023-08-07 10:29:26 +0700

    LAB7 - 8

--- Assignment/app/src/main/java/com/example/assignment/Interface/ItemClickListener.java
+++
@@ -1,6 +0,0 @@
-package com.example.assignment.Interface;
-
-public interface ItemClickListener {
-    void onItemClick(int position);
-
-}
--- Assignment/app/src/main/java/com/example/assignment/URLServer/PathURLServer.java
+++
@@ -1,9 +0,0 @@
-package com.example.assignment.URLServer;
-
-public class PathURLServer {
-    private static String baseURL="https://aojiro.000webhostapp.com/MOB403/";
-
-    public static String getBaseURL() {
-        return baseURL;
-    }
-}
